6a56a846680684d68335442def0115ebb1fc1d75
Sahrial Ardians <sahrialardians@gmail.com>
2021-06-26 13:43:45 +0700

    edit welcome message and workflow

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld msg="Deploying Vue App with AWS S3 and AWS CloudFront" />
   </div>
 </template>
 
